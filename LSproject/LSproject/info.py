4db43911288147a2dfa3345161a3955b45abc3ed
Shalini Singh <107787870+sha7186@users.noreply.github.com>
2023-12-02 15:30:18 +0530

    Add files via upload

---
+++ LSproject/LSproject/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'djangoproject7355@gmail.com'
+EMAIL_HOST_PASSWORD = 'ccawiaazcywndxry'
+EMAIL_PORT = 587
---
+++ LSproject/authentication/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+return redirect('home')
